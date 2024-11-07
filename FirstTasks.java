5f185f17c0a4b059737d107e396c9a36be1b1a56
joosepkallaste <joosep.kallaste@gmail.com>
2024-11-07 12:38:34 +0200

    Create FirstTasks.java

---
+++ FirstTasks.java
@@ -0,0 +1,59 @@
+public class FirstTasks {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Number1();
+        Number2();
+        PrintName();
+        PrintAge();
+        PrintCity();
+        PrintMorning();
+        PrintAfternoon();
+        PrintEvening();
+        FavoriteNumber();
+        TeamPoints();
+    }
+    public static void Number1() {
+        int var1 = 5;
+        System.out.println(var1);
+    }
+    public static void Number2() {
+        int varA = 10, varB = 15, varC = 20;
+        System.out.println(varA);
+        System.out.println(varB);
+        System.out.println(varC);
+    }
+
+    public static void PrintName() {
+        System.out.println("Name: Joosep");
+    }
+
+    public static void PrintAge() {
+        System.out.println("Age: 20");
+    }
+
+    public static void PrintCity() {
+        System.out.println("City: Tallinn");
+    }
+
+    public static void PrintMorning() {
+        System.out.println("Morning: Sleep");
+    }
+
+    public static void PrintAfternoon() {
+        System.out.println("Afternoon: Study");
+    }
+
+    public static void PrintEvening() {
+        System.out.println("Evening: Drinking wit friends");
+    }
+
+    public static void FavoriteNumber() {
+        int result = 8;
+        System.out.println("My favorite number is: " + result);
+    }
+
+    public static void TeamPoints() {
+        int var7 = 50;
+        System.out.println(var7);
+    }
+}
