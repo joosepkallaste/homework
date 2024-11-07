a5bdd6b218f2b3f6dcbcdecd0c00255c9aac63f5
joosepkallaste <joosep.kallaste@gmail.com>
2024-11-07 12:40:37 +0200

    Create PrintNumber2.java

---
+++ PrintNumber2.java
@@ -0,0 +1,8 @@
+public class PrintNumber2 {
+    public static void main(String[] args) {
+        int varA = 10, varB = 15, varC = 20;
+        System.out.println(varA);
+        System.out.println(varB);
+        System.out.println(varC);
+    }
+}
