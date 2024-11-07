e7e5304a4db2885fd843147834f70863615a17a3
joosepkallaste <joosep.kallaste@gmail.com>
2024-11-07 12:34:48 +0200

    Create Compare.java

---
+++ Compare.java
@@ -0,0 +1,11 @@
+public class Compare {
+    public static void main(String[] args) {
+        int c1 = 25;
+        int c2 = 39;
+        System.out.println("25 != 39: " + (c1 != c2));
+        System.out.println("25 < 39: " + (c1 < c2));
+        System.out.println("25 <= 39: " + (c1 <= c2));
+
+
+    }
+}
