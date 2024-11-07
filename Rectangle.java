944d41741cec9027c6672fad36612ebee9cc51f7
joosepkallaste <joosep.kallaste@gmail.com>
2024-11-07 12:40:58 +0200

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,14 @@
+public class Rectangle {
+    public static void main(String[] args) {
+        double width = 5.5;
+        double height = 8.5;
+
+
+        double area = (width * height);
+        System.out.println("Area = " +area);
+        double perimeter = (2*(width + height));
+        System.out.println("Perimeter = " +perimeter);
+
+
+    }
+}
