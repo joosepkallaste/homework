a46f96cea725430fb7ec0bf17a87c2adfd578ef5
joosepkallaste <joosep.kallaste@gmail.com>
2024-11-07 12:42:31 +0200

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,13 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 13;
+        a = a +8;
+        b = b -8;
+
+
+        System.out.println("a = "+a +", b = "+b);
+
+
+    }
+}
