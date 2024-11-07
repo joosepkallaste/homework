63666e1c580cdc0236cbd303f1078da6cbcb26a6
joosepkallaste <joosep.kallaste@gmail.com>
2024-11-07 12:39:03 +0200

    Create MyDay.java

---
+++ MyDay.java
@@ -0,0 +1,8 @@
+public class MyDay {
+    public static void main(String[] args) {
+        System.out.println("Morning: Sleep");
+        System.out.println("Afternoon: Study");
+        System.out.println("Evening: Drinking wit friends");
+
+    }
+}
