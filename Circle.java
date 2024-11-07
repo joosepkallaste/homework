351bd73eaac612439d0046460c6d9e7a0dc496a8
joosepkallaste <joosep.kallaste@gmail.com>
2024-11-07 12:34:01 +0200

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,8 @@
+public class Circle {
+    public static void main(String[] args) {
+        double var1 = Math.PI;
+        System.out.println(Math.PI * (7.5*2));
+        System.out.println(Math.pow(7.5, 2) * var1);
+
+    }
+}
