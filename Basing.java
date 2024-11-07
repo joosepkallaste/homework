080db3907ed4fff58c005b6611929376acebe1ed
joosepkallaste <joosep.kallaste@gmail.com>
2024-11-07 12:33:19 +0200

    Create Basing.java

---
+++ Basing.java
@@ -0,0 +1,14 @@
+public class Basing {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = 4;
+        int result1 = a-b-c;
+        System.out.println(result1);
+        long d = 5;
+        long e = 6;
+        long f = 7;
+        double result2 = (d*e)/f;
+        System.out.println(result2);
+    }
+}
