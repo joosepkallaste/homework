50417122e792ab2593408d9d1daf571fbd1888d0
joosepkallaste <joosep.kallaste@gmail.com>
2024-12-15 18:55:09 +0200

    Create Task9.java

---
+++ Task9.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class task9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        float tribe = number / 4;
+
+
+      //  for (int i = 0; i < 4; i ++){
+        //    if ()
+          //  for (int j = 0; j < number; j ++){
+            //    if (i > 0){
+                    System.out.print("*          ");
+
+              //  } else if (i > 1) {
+                    // Upper middle of the wave
+                    System.out.print(" *       * ");
+                //} else if (i == 2) {
+                    // Lower middle of the wave
+                  //  System.out.print(j % 2 == 0 ? "  *    *   ");
+                //} else {
+                    // Bottom of the wave
+                  //  System.out.print(j % 2 == 0 ? "    *      ");
+                }
+            }
+            //System.out.println();
+
+
+            //}
+
+        //}
+
+
+    //}
+
